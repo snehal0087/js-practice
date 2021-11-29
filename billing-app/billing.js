18465df9799a832b2b984f65a7ffb6c893213b16
snehal <snehal.m.0087@gmail.com>
2021-11-29 15:47:56 +0530

    updated code

--- billing-app/billing.js
+++ billing-app/billing.js
@@ -12,27 +12,37 @@ fetch('products.json')
 function appendData(data) {
     console.log(data);
     let productTableRow = document.getElementById("products");
-
-
+    let count=0;
     //Itetare over data 
     for (let i = 0; i < data.length; i++) {
-        console.log(data[i].productName + "  " + data[i].productPrice);
-
-       let prodRow = document.createElement('tr');
-         let prodCell = document.createElement('td');
-         let prodPriceCell = document.createElement('td');
-         let addButtonCell = document.createElement('td');
-         let removeButtonCell = document.createElement('td');
-         prodCell.innerHTML = data[i].productName;
-         prodPriceCell.innerHTML = data[i].productPrice;
-       //  addButtonCell.innerHTML = "+";
-        // removeButtonCell.innerHTML = "-";
-         prodRow.appendChild(prodCell);
-         prodRow.appendChild(prodPriceCell);
-         prodRow.appendChild(addButtonCell);
-         prodRow.appendChild(removeButtonCell);
+        // console.log(data[i].productName + "  " + data[i].productPrice);
+        let productname = data[i].productName;
+        let prodRow = document.createElement('tr');
+        let prodCell = document.createElement('td');
+        let prodPriceCell = document.createElement('td');
+        let addButtonCell = document.createElement('td');
+        let removeButtonCell = document.createElement('td');
+        prodCell.innerHTML = data[i].productName;
+        prodPriceCell.innerHTML = data[i].productPrice;
+        addButtonCell.innerHTML = "<button id='add'>+</button>";
+        removeButtonCell.innerHTML = "<button>-</button>";
+        
+        addButtonCell.addEventListener("click",function(){
+            console.log(data[i].productName+ "  "+ data[i].productPrice+"   "+(++count));
+          });
+        prodRow.appendChild(prodCell);
+        prodRow.appendChild(prodPriceCell);
+        prodRow.appendChild(addButtonCell);
+        prodRow.appendChild(removeButtonCell);
         productTableRow.appendChild(prodRow);
+
+        
 
     }
 
+    
+}
+
+function addToBill() {
+    console.log("productname");
 }
